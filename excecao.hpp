0dabbc6fac7620d7e78dd46759ede4d25355a374
Samuel Vasconcelos <69234519+samucavm2@users.noreply.github.com>
2023-12-15 14:38:43 -0300

    Update excecao.hpp

--- excecao.hpp
+++ excecao.hpp
@@ -4,18 +4,70 @@
 #ifndef EXCECAO_HPP
 #define EXCECAO_HPP
 
-
-
-
 class EscolhaInvalida : public std::exception{
 private:
   string escolha;      
-  std::string mensagem;  
+  string mensagem;  
 public:
 
 EscolhaInvalida(string t){
   escolha = t;
-  mensagem = "A opção escolhida não existe.\nDigite uma das opçoes abaixo:\n";
+  mensagem = "A opção escolhida '"+ escolha +"' não existe.\nDigite uma das opçoes abaixo:\n";
+}
+
+  const char* what() const throw() override{
+    return mensagem.c_str();
+  }
+};
+
+class CampoInvalido : public std::exception{
+private:
+  string digitado;      
+  string mensagem;  
+  string tipo;
+  string caracteres;
+public:
+
+CampoInvalido(string _digitado, string _tipo, string _caracteres){
+  digitado = _digitado;
+  tipo = _tipo;
+  caracteres = _caracteres;
+  mensagem = "" +tipo+ ": '" +digitado+ "' inválido. Diferente de " +caracteres+ " caracteres.\n";
+}
+
+  const char* what() const throw() override{
+    return mensagem.c_str();
+  }
+};
+
+
+class CaractereInvalido : public std::exception{
+private:
+  string digitado;      
+  string mensagem;  
+  string tipo;
+public:
+
+CaractereInvalido(string _digitado, string _tipo){
+  digitado = _digitado;
+  tipo = _tipo;
+mensagem = "" +tipo+ ": '" +digitado+ "' inválido. " +tipo+ " só deve conter caracteres de letras.\n";
+}
+
+  const char* what() const throw() override{
+    return mensagem.c_str();
+  }
+};
+
+class CampoVazio : public std::exception{
+private:   
+  string mensagem;  
+  string tipo;
+public:
+
+CampoVazio(string _tipo){
+  tipo = _tipo;
+mensagem = "Este campo '"+tipo+"' deve ser preenchido.";
 }
 
   const char* what() const throw() override{
